1df786d9c81acceb8657ed168c197ad523c188ee
BlisS <fixtergeek@gmail.com>
2023-05-27 19:33:51 -0600

    testing prisma again

--- src/routes/google/callback/index.tsx
+++ src/routes/google/callback/index.tsx
@@ -3,55 +3,90 @@ import {
   type RequestEvent,
   type RequestHandler,
 } from "@builder.io/qwik-city";
+import { PrismaClient } from "@prisma/client";
 // import { PrismaClient } from "@prisma/client";
 // import { type UserType } from "~/db/zod";
 
 const googleTokensURL = "https://oauth2.googleapis.com/token?";
 
-// type UserDataType = {
-//   id: string;
-//   email: string;
-//   verified_email: boolean;
-//   picture: string;
-// };
+type UserDataType = {
+  id: string;
+  email: string;
+  verified_email: boolean;
+  picture: string;
+};
+
+const getOrCreateUserFromGoogle = server$(
+  async ({ token_type, access_token, refresh_token }) => {
+    const options = {
+      headers: {
+        Accept: "application/json",
+        Authorization: `${token_type} ` + access_token,
+      },
+      method: "get",
+    };
+    const url = "https://www.googleapis.com/oauth2/v2/userinfo";
+    const resp = await fetch(url, options);
+    const data: UserDataType = await resp.json();
+    const newUserData = {
+      name: undefined,
+      email: data.email,
+      picture: data.picture,
+      access_token,
+      refresh_token,
+      provider: "google",
+    };
+    const db = new PrismaClient();
+    return await db.user.upsert({
+      where: {
+        id: newUserData.id,
+        email: newUserData.email,
+      },
+      update: {
+        name: newUserData.name,
+        email: newUserData.email,
+        picture: newUserData.picture,
+        access_token: newUserData.access_token,
+        refresh_token: newUserData.refresh_token,
+        provider: newUserData.provider,
+      },
+      create: {
+        name: newUserData.name,
+        email: newUserData.email,
+        picture: newUserData.picture,
+        access_token: newUserData.access_token,
+        refresh_token: newUserData.refresh_token,
+        provider: newUserData.provider,
+      },
+    });
+  }
+);
 
 const createSessionAndRedirect = server$(
   async ({
-    // access_token,
+    access_token,
     request,
     redirectURL = "/",
-  }: // refresh_token,
-  // token_type,
-  {
+    token_type,
+    refresh_token,
+  }: {
     refresh_token: string;
     token_type: string;
     access_token: string;
     request: RequestEvent<QwikCityPlatform>;
     redirectURL: string;
   }) => {
-    // const options = {
-    //   headers: {
-    //     Accept: "application/json",
-    //     Authorization: `${token_type} ` + access_token,
-    //   },
-    //   method: "get",
-    // };
-    // const url = "https://www.googleapis.com/oauth2/v2/userinfo";
-    // const resp = await fetch(url, options);
-    // const data: UserDataType = await resp.json();
-    // const user = await upsertUser({
-    //   email: data.email,
-    //   picture: data.picture,
-    //   access_token,
-    //   refresh_token,
-    //   provider: "google",
-    // });
+    const user = await getOrCreateUserFromGoogle({
+      access_token,
+      token_type,
+      refresh_token,
+    });
     // // set cookie
-    // await request.cookie.set("userId", user.id, {
-    //   path: "/",
-    //   httpOnly: true,
-    //   sameSite: "strict",
-    // });
+    await request.cookie.set("userId", user.id, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+    });
 
     throw request.redirect(303, redirectURL);
     // request.json(200, user);
